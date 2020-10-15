1fe8f28f8855eac07f7099db9b65ad92b04cadc7
username@email.com <1912339826@qq.com>
2020-10-15 18:23:46 +0800

    2020年10月15日18:23:14

---
+++ dome/location/location.js
@@ -0,0 +1,7 @@
+function getLocation() {
+    return {
+        // 数据为Number格式
+        X: 118.82693904485,
+        Y: 32.033087926105
+    }
+}
--- dome/src/js/About_Us.js
+++ dome/src/js/About_Us.js
@@ -1,26 +1,41 @@
 $("#About_Us").click(function (e) {
-    e.preventDefault();
-    $(".tab_control").hide()
-  });
-  // 点击产品中心
-  $("#product_center").click(function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    if ($(".tab_control").is(":hidden")) {
-      $(".tab_control").show()
-    } else {
-      $(".tab_control").hide()
-    }
-  
-  });
-  $(".tab_control").click(function (e) {
-    e.preventDefault();
-    e.stopPropagation();
+  e.preventDefault();
+  $(".tab_control").hide()
+});
+// 点击产品中心
+$("#product_center").click(function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  if ($(".tab_control").is(":hidden")) {
     $(".tab_control").show()
-  });
-  
-  $(".search_button").click(function (e) {
-    e.preventDefault();
-    $(this).hide()
-    $(".search_input").show()
-  });
+  } else {
+    $(".tab_control").hide()
+  }
+
+});
+$(".tab_control").click(function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  $(".tab_control").show()
+});
+
+$(".search_button").click(function (e) {
+  e.preventDefault();
+  $(this).hide()
+  $(".search_input").show()
+});
+
+var map = new BMap.Map("container");
+// 创建地图实例  
+var point = new BMap.Point(getLocation().X, getLocation().Y);
+
+// 创建点坐标  
+window.setTimeout(() => {
+  map.addControl(new BMap.NavigationControl());
+  map.addControl(new BMap.ScaleControl());
+  map.addControl(new BMap.OverviewMapControl());
+  map.centerAndZoom(point, 20);
+  var marker = new BMap.Marker(point); // 创建标注    
+  map.addOverlay(marker); // 将标注添加到地图中 
+  // 初始化地图，设置中心点坐标和地图级别  
+}, 2000);
